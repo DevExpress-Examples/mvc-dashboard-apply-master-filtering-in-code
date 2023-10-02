b97420c9d8f93aeed1346d24e3a84d157c2ea61d
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 17:56:25 +0400

    Change example status to out of maintenance

--- CS/MVCxDashboard_ApplyMasterFilter/Controllers/DefaultDashboardController.cs
+++
@@ -1,7 +0,0 @@
-﻿﻿using DevExpress.DashboardWeb.Mvc;
-
-namespace MVCxDashboard_ApplyMasterFilter.Controllers {
-    public class DefaultDashboardController : DashboardController {
-
-    }
-}
--- CS/MVCxDashboard_ApplyMasterFilter/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCxDashboard_ApplyMasterFilter.MvcApplication" Language="C#" %>
